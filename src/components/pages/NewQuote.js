115d910da637d2b42edbb8fb2d2a06236f1746c3
Edilbek <edil.talantbekov@gmail.com>
2022-12-23 11:33:51 +0600

    add useHistory push

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 
 import QuoteForm from '../../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const history = useHistory();
+  
   const addQuiteHandler = quoteDate => {
     console.log(quoteDate);
+
+    history.push('/quotes');
   };
 
   return (
