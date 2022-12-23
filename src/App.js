c0f5840f04c631480ab64ae9eae770b6d4cc1316
Edilbek <edil.talantbekov@gmail.com>
2022-12-23 11:09:08 +0600

    add wrapper

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import AllQuotes from "./components/pages/AllQuotes";
 import NewQuote from "./components/pages/NewQuote";
 import QuoteDetail from "./components/pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/quotes' />
-      </Route>
-      <Route path='/quotes' exact component={AllQuotes} />
-      <Route path='/quotes/:quoteId' component={QuoteDetail} />
-      <Route path='/new-quote' component={NewQuote} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route path='/quotes' exact component={AllQuotes} />
+        <Route path='/quotes/:quoteId' component={QuoteDetail} />
+        <Route path='/new-quote' component={NewQuote} />
+      </Switch>
+    </Layout>
   );
 }
 
